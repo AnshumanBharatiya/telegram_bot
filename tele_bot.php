83cc4511865f9de7e83a5ced0d8372446126bc9b
anshuman bharatiya <44137214+AnshumanBharatiya@users.noreply.github.com>
2021-08-18 16:09:17 +0530

    Add files via upload

---
+++ tele_bot.php
@@ -0,0 +1,25 @@
+<!-- 
+https://api.telegram.org/bot1931048753:AAGvtsbxNWJ_8zryDQhXRhUORAnI85kxv4c/getMe
+https://api.telegram.org/bot1931048753:AAGvtsbxNWJ_8zryDQhXRhUORAnI85kxv4c/sendMessage
+ -->
+
+
+<!-- telegram main link for apis and bot -  https://core.telegram.org/bots/api -->
+
+
+<!-- bot reply to user through this link -->
+<!--  https://api.telegram.org/bot1931048753:AAGvtsbxNWJ_8zryDQhXRhUORAnI85kxv4c/sendMessage?chat_id=920619762&text=how may i help you -->
+
+
+
+<!-- (bot) get an update data through this link, which are send by user to the bot-->
+<!-- https://api.telegram.org/bot1931048753:AAGvtsbxNWJ_8zryDQhXRhUORAnI85kxv4c/getUpdates -->
+
+
+
+<!-- Use this token to access the HTTP API:
+1931048753:AAGvtsbxNWJ_8zryDQhXRhUORAnI85kxv4c -->
+
+
+
+
