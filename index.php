32086dadb26b1e6f660b4842b52224ee1697b35c
anshuman bharatiya <44137214+AnshumanBharatiya@users.noreply.github.com>
2021-08-18 15:48:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
 
+$input = file_get_contents('php://input');
+$data = json_decode($input);
+$chat_id = $data->message->chat->id;
+$text = $data->message->text;
+$text = $text."Webhook Reply";
+
+$token = "1931048753:AAGvtsbxNWJ_8zryDQhXRhUORAnI85kxv4c";
+$url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$text";
+
+file_get_contents($url);
+?>
